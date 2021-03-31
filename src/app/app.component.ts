f63af8377c2bb6f688b2199ca1ff1c410035565a
pco <pco@paywith.co.kr>
2021-04-01 00:17:46 +0900

    app component 템플릿 바인딩 후 커밋

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
+import { Product } from './model/product.model';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'inventory-app';
+  product: Product;
+
+  constructor() {
+    this.product = new Product(
+      'NICEHAT',
+      'A Nice Black Hat',
+      '/assets/images/products/black-hat.jpg',
+      ['Men', 'Accessories', 'Hats'],
+      29.99
+    );
+  }
 }
